a5610547424ce6b6deae3abc7d813f20e8d131c7
MarcelBasa <marcel.basa@interia.pl>
2024-09-02 19:54:07 +0200

    Add class DroneGameMode

---
+++ Source/Drone_simulator/GameModes/DroneGameMode.cpp
@@ -0,0 +1,2 @@
+#include "DroneGameMode.h"
+
---
+++ Source/Drone_simulator/GameModes/DroneGameMode.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "DroneGameMode.generated.h"
+
+
+UCLASS()
+class DRONE_SIMULATOR_API ADroneGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
