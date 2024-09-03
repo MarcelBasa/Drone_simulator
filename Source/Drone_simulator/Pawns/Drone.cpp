266ac88117ae06b21b43a5c95c763efbeb7bbdbb
MarcelBasa <marcel.basa@interia.pl>
2024-09-03 15:08:10 +0200

    Add Camera with controll and start make player movement

--- Source/Drone_simulator/Pawns/Drone.cpp
+++ Source/Drone_simulator/Pawns/Drone.cpp
@@ -1,9 +1,19 @@
 #include "Drone.h"
+#include "Camera/CameraComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+
 
 ADrone::ADrone()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	DroneMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Drone mesh"));
+	DroneMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	RootComponent = DroneMesh;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(RootComponent);
+	Camera->bUsePawnControlRotation = true;
 }
 
 void ADrone::BeginPlay()
@@ -22,5 +32,38 @@ void ADrone::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ThisClass::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &ThisClass::MoveRight);
+	PlayerInputComponent->BindAxis(TEXT("Turn"), this, &ThisClass::Turn);
+	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &ThisClass::LookUp);
 }
 
+void ADrone::MoveForward(float Value)
+{
+	if (Controller != nullptr && Value != 0.f)
+	{
+		const FRotator YawRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+		const FVector Direction(FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X));
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void ADrone::MoveRight(float Value)
+{
+	if (Controller != nullptr && Value != 0.f)
+	{
+		const FRotator YawRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+		const FVector Direction(FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y));
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void ADrone::Turn(float Value)
+{
+	AddControllerYawInput(Value);
+}
+
+void ADrone::LookUp(float Value)
+{
+	AddControllerPitchInput(Value);
+}
--- Source/Drone_simulator/Pawns/Drone.h
+++ Source/Drone_simulator/Pawns/Drone.h
@@ -4,6 +4,9 @@
 #include "GameFramework/Pawn.h"
 #include "Drone.generated.h"
 
+class USkeletalMeshComponent;
+class UCameraComponent;
+
 UCLASS()
 class DRONE_SIMULATOR_API ADrone : public APawn
 {
@@ -15,8 +18,18 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+	void Turn(float Value);
+	void LookUp(float Value);
+
 public:	
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+	UPROPERTY(EditAnywhere)
+		USkeletalMeshComponent* DroneMesh = nullptr;
+	UPROPERTY(EditAnywhere)
+		UCameraComponent* Camera = nullptr;
 };
