a65487bebd9174d3a5d8f3092eab3fab9c65eede
MarcelBasa <marcel.basa@interia.pl>
2024-09-14 21:28:25 +0200

    Add HUD Class for drone

---
+++ Source/Drone_simulator/HUD/DroneHUD.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "DroneHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRONE_SIMULATOR_API ADroneHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
