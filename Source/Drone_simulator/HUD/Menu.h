d24a4e4d6d5e50e21d9f7c4b40718ab5402f6c63
MarcelBasa <marcel.basa@interia.pl>
2024-09-15 23:42:12 +0200

    Add Menu widget class

---
+++ Source/Drone_simulator/HUD/Menu.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Menu.generated.h"
+
+
+
+UCLASS()
+class DRONE_SIMULATOR_API UMenu : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
