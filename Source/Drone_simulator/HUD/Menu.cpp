df1794237fd767e149bf65fabd2d391f071ccba8
MarcelBasa <marcel.basa@interia.pl>
2024-09-18 20:12:05 +0200

    Switching between lidar and orto map

--- Source/Drone_simulator/HUD/Menu.cpp
+++ Source/Drone_simulator/HUD/Menu.cpp
@@ -1,2 +1,35 @@
 #include "Menu.h"
+#include "Components/Button.h"
+#include "Kismet/GameplayStatics.h"
 
+void UMenu::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	QuitButton->OnClicked.AddDynamic(this, &ThisClass::Quit);
+	LidarButton->OnClicked.AddDynamic(this, &ThisClass::ChooseLidar);
+	OrtofotomapaButton->OnClicked.AddDynamic(this, &ThisClass::ChooseOrtofotomapa);
+}
+
+void UMenu::Quit()
+{
+	APlayerController* PlayerController = Cast<APlayerController>(UGameplayStatics::GetPlayerController(GetWorld(), 0));
+	if (PlayerController)
+		PlayerController->ConsoleCommand("quit");
+}
+
+void UMenu::ChooseLidar()
+{
+	LoadMap("Szczecin_LIDAR.umap");
+}
+
+void UMenu::ChooseOrtofotomapa()
+{
+	LoadMap("Szczecin_Ortofotomapa.umap");
+}
+
+void UMenu::LoadMap(FString mapName)
+{
+	FString mapPath = "D:/UE_5_projekty_D/Drone_simulator/Content/Maps/" + mapName;
+	UGameplayStatics::OpenLevel(GetWorld(), FName(mapPath));
+}
--- Source/Drone_simulator/HUD/Menu.h
+++ Source/Drone_simulator/HUD/Menu.h
@@ -4,11 +4,28 @@
 #include "Blueprint/UserWidget.h"
 #include "Menu.generated.h"
 
-
+class UButton;
 
 UCLASS()
 class DRONE_SIMULATOR_API UMenu : public UUserWidget
 {
 	GENERATED_BODY()
 	
+public:
+	UPROPERTY(meta = (BindWidget))
+		UButton* QuitButton;
+	UPROPERTY(meta = (BindWidget))
+		UButton* LidarButton;
+	UPROPERTY(meta = (BindWidget))
+		UButton* OrtofotomapaButton;
+
+protected:
+	virtual void NativeConstruct() override;
+	UFUNCTION()
+		void Quit();
+	UFUNCTION()
+		void ChooseLidar();
+	UFUNCTION()
+		void ChooseOrtofotomapa();
+	void LoadMap(FString mapName);
 };
