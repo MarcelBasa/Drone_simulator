bdfe961950c2f086b7d66e75e6a7fbc584e1c3a6
MarcelBasa <marcel.basa@interia.pl>
2024-10-24 19:16:25 +0200

    add back function

--- Source/Drone_simulator/HUD/CameraMenu.cpp
+++ Source/Drone_simulator/HUD/CameraMenu.cpp
@@ -1,2 +1,25 @@
 #include "CameraMenu.h"
+#include "Kismet/GameplayStatics.h"
+#include "Drone_simulator/Controllers/DroneController.h"
+#include "Components/Button.h"
 
+
+void UCameraMenu::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	BackButton->OnClicked.AddDynamic(this, &ThisClass::Back);
+
+	DroneController = Cast<ADroneController>(UGameplayStatics::GetPlayerController(GetWorld(), 0));
+	if (DroneController)
+	{
+		DroneController->SetInputMode(FInputModeUIOnly());
+		DroneController->bShowMouseCursor = true;
+	}
+}
+
+void UCameraMenu::Back()
+{
+	if (DroneController)
+		DroneController->HandleSetMenu();
+}
--- Source/Drone_simulator/HUD/CameraMenu.h
+++ Source/Drone_simulator/HUD/CameraMenu.h
@@ -4,12 +4,23 @@
 #include "Blueprint/UserWidget.h"
 #include "CameraMenu.generated.h"
 
-/**
- * 
- */
+class UButton;
+class ADroneController;
+
 UCLASS()
 class DRONE_SIMULATOR_API UCameraMenu : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+public:
+	UPROPERTY(meta = (BindWidget))
+		UButton* BackButton;
+
+protected:
+	virtual void NativeConstruct() override;
+	UFUNCTION()
+		void Back();
+
+private:
+	ADroneController* DroneController = nullptr;
 };
